72926e61493da882b3ce5a6e1b9d59915483bf39
cwilliston <carolynnwilliston@gmail.com>
2022-08-22 20:29:35 -0500

    GA #1 complete, or at least working

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -47,11 +47,31 @@ namespace TechJobsConsole
 
             foreach (Dictionary<string, string> row in AllJobs)
             {
-                string aValue = row[column];
+                string aValue = row[column].ToLower();
 
                 if (aValue.Contains(value))
                 {
                     jobs.Add(row);
+                }
+            }
+
+            return jobs;
+        }
+
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData();
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                foreach (KeyValuePair<string, string> pair in row)
+                {
+                    if (pair.Value.ToLower().Contains(value))
+                    {
+                        if (!jobs.Contains(row)) { jobs.Add(row); }
+                    }
                 }
             }
 
